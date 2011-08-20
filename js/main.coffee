53b65e95ad213df701ada12a2dd6911081ccf1ef
Brenton Partridge <bapartridge@gmail.com>
2011-08-19 21:02:35 -0700

    initial backbone couch connector wiring

--- js/main.coffee
+++ js/main.coffee
@@ -1,1 +1,6 @@
 $('#main').text 'woohoo'
+
+$ ->
+	Backbone.couch_connector.config.db_name = "posters"
+	Backbone.couch_connector.config.ddoc_name = ''
+	Backbone.couch_connector.enable_changes = true
--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 (function() {
   $('#main').text('woohoo');
+  $(function() {
+    Backbone.couch_connector.config.db_name = "posters";
+    Backbone.couch_connector.config.ddoc_name = '';
+    return Backbone.couch_connector.enable_changes = true;
+  });
 }).call(this);

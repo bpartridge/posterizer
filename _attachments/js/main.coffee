ec37c2fd0ffd2100fd072be9d096e0adcb8879a1
Brenton Partridge <bapartridge@gmail.com>
2011-08-21 00:07:06 -0700

    jquery.mobile UI for home and task-table pages

--- _attachments/js/main.coffee
+++ _attachments/js/main.coffee
@@ -1,106 +1,146 @@
+Backbone.couch_connector.config.db_name = 'posters'
+Backbone.couch_connector.config.ddoc_name = 'app'
+
+# Enables Mustache.js-like templating.
+_.templateSettings = `{interpolate : /\{\{(.+?)\}\}/g}`
+
+reapplyStyles = (child) ->
+  el = $(child).closest('[data-role="page"]')
+  # console.log "reapplyStyles", child, el
+  try
+    el.find('ul[data-role]').listview('refresh')
+    el.find('div[data-role="fieldcontain"]').fieldcontain('refresh')
+    el.find('button[data-role="button"]').button('refresh')
+    el.find('input,textarea').textinput('refresh')
+    el.page()
+  catch error
+    # console.log "Error in reapplyStyles", error
+
+class State extends Backbone.Model
+  defaults:
+    username: null
+    task_id: null
+    task_title: 'unknown'
+  initialize: ->
+    @bind 'all', -> console.log arguments # for debugging
+
+state = new State
+
+class Task extends Backbone.Model
+  defaults:
+    type: 'task'
+    title: 'Untitled Task'
+
+class TasksWithEventCounts extends Backbone.Collection
+  db:
+    view: 'tasks_with_event_counts'
+    group: true
+  url: '/tasks_with_event_counts'
+  model: Task
+  comparator: (task) -> return task.get 'title'
+
+tasksWithEventCounts = new TasksWithEventCounts
+
+class TaskEvent extends Backbone.Model
+  defaults:
+    type: 'task_event'
+    task_id: null
+    task_title: null
+
+class TaskTableEntryView extends Backbone.View
+  tagName: $('#task-table-entry-template')[0].nodeName
+  template: _.template($('#task-table-entry-template').html())
+  initialize: ->
+    super(arguments)
+    # console.log this, this.model, options
+    @render()
+  render: =>
+    # console.log "Rendering", this, "to", @el, "parent", $(@el).parent()
+    content = @model.toJSON()
+    $(@el).html @template(content)
+  events:
+    "click a": "onLink"
+  onLink: =>
+    # console.log "Setting state for", @model
+    state.set
+      task_id: @model.id
+      task_title: @model.get 'task_title'
+    true # follow link, which will bring up an editing page
+
+class UpdatingCollectionView extends Backbone.View
+  initialize: (options) ->
+    @childViewConstructor ||= options.childViewConstructor
+    # console.log this
+    throw "no child view constructor" unless @childViewConstructor
+    @_childViews = []
+    @collection.each @add
+    @collection.bind 'add', @add
+    @collection.bind 'remove', @remove
+    @collection.bind 'reset', @reset
+  reset: =>
+    if @_rendered
+      $(view.el).remove() for view in @_childViews
+    @collection.each (model) => @add model
+    null
+  add: (model) =>
+    childView = new @childViewConstructor({model:model})
+    @_childViews.push childView
+    if @_rendered
+      @renderChildView childView
+    reapplyStyles @el
+  remove: (model) =>
+    viewToRemove = _(@_childViews).detect (cv) -> cv.model is model
+    @_childViews = _(@_childViews).without viewToRemove
+    if @_rendered
+      $(viewToRemove.el).remove()
+  render: =>
+    @_rendered = true
+    @renderChildView view for view in @_childViews
+    reapplyStyles @el
+  renderChildView: (childView) =>
+    $(@el).append childView.el
+    childView.render
+
+class TaskTableView extends UpdatingCollectionView
+  childViewConstructor: TaskTableEntryView
+  initialize: ->
+    super(arguments)
+    # console.log "TaskTableView initialized", this
+
+class TaskEventAddView extends Backbone.View
+  initialize: ->
+    super(arguments)
+    state.bind 'change', @render
+  render: =>
+    if not state.get 'username'
+      $(@el).text """
+        You must log in below before adding activity to a task."""
+    else
+      $(@el).text """
+        Adding activity as #{state.get 'username'} to task: 
+        #{state.get 'task_title'}"""
+
+class HomeView extends Backbone.View
+  initialize: ->
+    super(arguments)
+    state.bind 'change', @render
+    @render()
+  render: =>
+    if state.get 'username'
+      @$('h2').text "Welcome, #{state.get 'username'}!"
+    else
+      @$('h2').text "Welcome to the Posterizer!"
+
 $ ->
-  Backbone.couch_connector.config.db_name = 'posters'
-  Backbone.couch_connector.config.ddoc_name = 'app'
+  views = []
+  views.push new HomeView
+    el: $('#home').find('.content')
+  views.push new TaskTableView
+    el: $('#task-table')
+    collection: tasksWithEventCounts
+  tasksWithEventCounts.fetch()
   
-  # Enables Mustache.js-like templating.
-  _.templateSettings = `{interpolate : /\{\{(.+?)\}\}/g}`
-  
-  app =
-    activePage: ->
-      $(".ui-page-active").get(0)
-
-    reapplyStyles: (el) ->
-      el.find('ul[data-role]').listview();
-      el.find('div[data-role="fieldcontain"]').fieldcontain();
-      el.find('button[data-role="button"]').button();
-      el.find('input,textarea').textinput();
-      el.page()
-
-    redirectTo: (page) ->
-      $.mobile.changePage page
-
-    goBack: ->
-      $.historyBack()
-  
-  class Task extends Backbone.Model
-    defaults:
-      type: 'task'
-      title: 'Untitled Task'
-  
-  class TasksWithEventCounts extends Backbone.Collection
-    db:
-      view: 'tasks_with_event_counts'
-      group: true
-    url: '/tasks_with_event_counts'
-    model: Task
-    comparator: (task) -> return task.get 'title'
-  
-  tasksWithEventCounts = new TasksWithEventCounts
-  
-  class TaskEvent extends Backbone.Model
-    defaults:
-      type: 'task_event'
-      task_id: null
-      task_title: null
-  
-  class UpdatingCollectionView extends Backbone.View
-    initialize: (options) ->
-      @childViewConstructor ||= options.childViewConstructor
-      console.log this
-      throw "no child view constructor" unless @childViewConstructor
-      @_childViews = []
-      @collection.each @add
-      @collection.bind 'add', @add
-      @collection.bind 'remove', @remove
-      @collection.bind 'reset', @reset
-    reset: =>
-      if @_rendered
-        $(view.el).remove() for view in @_childViews
-      @collection.each (model) => @add model
-      null
-    add: (model) =>
-      childView = new @childViewConstructor({model:model})
-      @_childViews.push childView
-      if @_rendered
-        @renderChildView childView
-    remove: (model) =>
-      viewToRemove = _(@_childViews).detect (cv) -> cv.model is model
-      @_childViews = _(@_childViews).without viewToRemove
-      if @_rendered
-        $(viewToRemove.el).remove()
-    render: =>
-      @_rendered = true
-      @renderChildView view for view in @_childViews
-    renderChildView: (childView) =>
-      $(@el).append childView.el
-      childView.render
-  
-  class TaskTableEntryView extends Backbone.View
-    tagName: $('#task-table-entry-template')[0].nodeName
-    template: _.template($('#task-table-entry-template').html())
-    initialize: (options) ->
-      super(options)
-      # console.log this, this.model, options
-      @render()
-    render: =>
-      console.log "Rendering", this, "to", @el
-      content = @model.toJSON()
-      $(@el).html @template(content)
-  
-  class TaskTableView extends UpdatingCollectionView
-    childViewConstructor: TaskTableEntryView
-    initialize: (options) ->
-      super(options)
-      @el = app.activePage()
-      @render()
-  
-  class HomeView extends Backbone.View
-    el: $('#home')
-    initialize: (options) ->
-      super(options)
-      @el = app.activePage()
-      @render()
-    render: =>
-      @$('[data-role=content]').text('Loaded')
-  
-  Backbone.history.start()
+  views.push new TaskEventAddView
+    el: $('#add-activity').find('.content')
+  $('body').live 'pagecreate', ->
+    view.render() for view in views
--- _attachments/js/main.js
+++ _attachments/js/main.js
@@ -1,4 +1,5 @@
 (function() {
+  var HomeView, State, Task, TaskEvent, TaskEventAddView, TaskTableEntryView, TaskTableView, TasksWithEventCounts, UpdatingCollectionView, reapplyStyles, state, tasksWithEventCounts;
   var __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) {
     for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; }
     function ctor() { this.constructor = child; }
@@ -7,192 +8,254 @@
     child.__super__ = parent.prototype;
     return child;
   }, __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
-  $(function() {
-    var HomeView, Task, TaskEvent, TaskTableEntryView, TaskTableView, TasksWithEventCounts, UpdatingCollectionView, app, tasksWithEventCounts;
-    Backbone.couch_connector.config.db_name = 'posters';
-    Backbone.couch_connector.config.ddoc_name = 'app';
-    _.templateSettings = {interpolate : /\{\{(.+?)\}\}/g};
-    app = {
-      activePage: function() {
-        return $(".ui-page-active").get(0);
-      },
-      reapplyStyles: function(el) {
-        el.find('ul[data-role]').listview();
-        el.find('div[data-role="fieldcontain"]').fieldcontain();
-        el.find('button[data-role="button"]').button();
-        el.find('input,textarea').textinput();
-        return el.page();
-      },
-      redirectTo: function(page) {
-        return $.mobile.changePage(page);
-      },
-      goBack: function() {
-        return $.historyBack();
-      }
+  Backbone.couch_connector.config.db_name = 'posters';
+  Backbone.couch_connector.config.ddoc_name = 'app';
+  _.templateSettings = {interpolate : /\{\{(.+?)\}\}/g};
+  reapplyStyles = function(child) {
+    var el;
+    el = $(child).closest('[data-role="page"]');
+    try {
+      el.find('ul[data-role]').listview('refresh');
+      el.find('div[data-role="fieldcontain"]').fieldcontain('refresh');
+      el.find('button[data-role="button"]').button('refresh');
+      el.find('input,textarea').textinput('refresh');
+      return el.page();
+    } catch (error) {
+
+    }
+  };
+  State = (function() {
+    __extends(State, Backbone.Model);
+    function State() {
+      State.__super__.constructor.apply(this, arguments);
+    }
+    State.prototype.defaults = {
+      username: null,
+      task_id: null,
+      task_title: 'unknown'
     };
-    Task = (function() {
-      __extends(Task, Backbone.Model);
-      function Task() {
-        Task.__super__.constructor.apply(this, arguments);
+    State.prototype.initialize = function() {
+      return this.bind('all', function() {
+        return console.log(arguments);
+      });
+    };
+    return State;
+  })();
+  state = new State;
+  Task = (function() {
+    __extends(Task, Backbone.Model);
+    function Task() {
+      Task.__super__.constructor.apply(this, arguments);
+    }
+    Task.prototype.defaults = {
+      type: 'task',
+      title: 'Untitled Task'
+    };
+    return Task;
+  })();
+  TasksWithEventCounts = (function() {
+    __extends(TasksWithEventCounts, Backbone.Collection);
+    function TasksWithEventCounts() {
+      TasksWithEventCounts.__super__.constructor.apply(this, arguments);
+    }
+    TasksWithEventCounts.prototype.db = {
+      view: 'tasks_with_event_counts',
+      group: true
+    };
+    TasksWithEventCounts.prototype.url = '/tasks_with_event_counts';
+    TasksWithEventCounts.prototype.model = Task;
+    TasksWithEventCounts.prototype.comparator = function(task) {
+      return task.get('title');
+    };
+    return TasksWithEventCounts;
+  })();
+  tasksWithEventCounts = new TasksWithEventCounts;
+  TaskEvent = (function() {
+    __extends(TaskEvent, Backbone.Model);
+    function TaskEvent() {
+      TaskEvent.__super__.constructor.apply(this, arguments);
+    }
+    TaskEvent.prototype.defaults = {
+      type: 'task_event',
+      task_id: null,
+      task_title: null
+    };
+    return TaskEvent;
+  })();
+  TaskTableEntryView = (function() {
+    __extends(TaskTableEntryView, Backbone.View);
+    function TaskTableEntryView() {
+      this.onLink = __bind(this.onLink, this);
+      this.render = __bind(this.render, this);
+      TaskTableEntryView.__super__.constructor.apply(this, arguments);
+    }
+    TaskTableEntryView.prototype.tagName = $('#task-table-entry-template')[0].nodeName;
+    TaskTableEntryView.prototype.template = _.template($('#task-table-entry-template').html());
+    TaskTableEntryView.prototype.initialize = function() {
+      TaskTableEntryView.__super__.initialize.call(this, arguments);
+      return this.render();
+    };
+    TaskTableEntryView.prototype.render = function() {
+      var content;
+      content = this.model.toJSON();
+      return $(this.el).html(this.template(content));
+    };
+    TaskTableEntryView.prototype.events = {
+      "click a": "onLink"
+    };
+    TaskTableEntryView.prototype.onLink = function() {
+      state.set({
+        task_id: this.model.id,
+        task_title: this.model.get('task_title')
+      });
+      return true;
+    };
+    return TaskTableEntryView;
+  })();
+  UpdatingCollectionView = (function() {
+    __extends(UpdatingCollectionView, Backbone.View);
+    function UpdatingCollectionView() {
+      this.renderChildView = __bind(this.renderChildView, this);
+      this.render = __bind(this.render, this);
+      this.remove = __bind(this.remove, this);
+      this.add = __bind(this.add, this);
+      this.reset = __bind(this.reset, this);
+      UpdatingCollectionView.__super__.constructor.apply(this, arguments);
+    }
+    UpdatingCollectionView.prototype.initialize = function(options) {
+      this.childViewConstructor || (this.childViewConstructor = options.childViewConstructor);
+      if (!this.childViewConstructor) {
+        throw "no child view constructor";
       }
-      Task.prototype.defaults = {
-        type: 'task',
-        title: 'Untitled Task'
-      };
-      return Task;
-    })();
-    TasksWithEventCounts = (function() {
-      __extends(TasksWithEventCounts, Backbone.Collection);
-      function TasksWithEventCounts() {
-        TasksWithEventCounts.__super__.constructor.apply(this, arguments);
-      }
-      TasksWithEventCounts.prototype.db = {
-        view: 'tasks_with_event_counts',
-        group: true
-      };
-      TasksWithEventCounts.prototype.url = '/tasks_with_event_counts';
-      TasksWithEventCounts.prototype.model = Task;
-      TasksWithEventCounts.prototype.comparator = function(task) {
-        return task.get('title');
-      };
-      return TasksWithEventCounts;
-    })();
-    tasksWithEventCounts = new TasksWithEventCounts;
-    TaskEvent = (function() {
-      __extends(TaskEvent, Backbone.Model);
-      function TaskEvent() {
-        TaskEvent.__super__.constructor.apply(this, arguments);
-      }
-      TaskEvent.prototype.defaults = {
-        type: 'task_event',
-        task_id: null,
-        task_title: null
-      };
-      return TaskEvent;
-    })();
-    UpdatingCollectionView = (function() {
-      __extends(UpdatingCollectionView, Backbone.View);
-      function UpdatingCollectionView() {
-        this.renderChildView = __bind(this.renderChildView, this);
-        this.render = __bind(this.render, this);
-        this.remove = __bind(this.remove, this);
-        this.add = __bind(this.add, this);
-        this.reset = __bind(this.reset, this);
-        UpdatingCollectionView.__super__.constructor.apply(this, arguments);
-      }
-      UpdatingCollectionView.prototype.initialize = function(options) {
-        this.childViewConstructor || (this.childViewConstructor = options.childViewConstructor);
-        console.log(this);
-        if (!this.childViewConstructor) {
-          throw "no child view constructor";
-        }
-        this._childViews = [];
-        this.collection.each(this.add);
-        this.collection.bind('add', this.add);
-        this.collection.bind('remove', this.remove);
-        return this.collection.bind('reset', this.reset);
-      };
-      UpdatingCollectionView.prototype.reset = function() {
-        var view, _i, _len, _ref;
-        if (this._rendered) {
-          _ref = this._childViews;
-          for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-            view = _ref[_i];
-            $(view.el).remove();
-          }
-        }
-        this.collection.each(__bind(function(model) {
-          return this.add(model);
-        }, this));
-        return null;
-      };
-      UpdatingCollectionView.prototype.add = function(model) {
-        var childView;
-        childView = new this.childViewConstructor({
-          model: model
-        });
-        this._childViews.push(childView);
-        if (this._rendered) {
-          return this.renderChildView(childView);
-        }
-      };
-      UpdatingCollectionView.prototype.remove = function(model) {
-        var viewToRemove;
-        viewToRemove = _(this._childViews).detect(function(cv) {
-          return cv.model === model;
-        });
-        this._childViews = _(this._childViews).without(viewToRemove);
-        if (this._rendered) {
-          return $(viewToRemove.el).remove();
-        }
-      };
-      UpdatingCollectionView.prototype.render = function() {
-        var view, _i, _len, _ref, _results;
-        this._rendered = true;
+      this._childViews = [];
+      this.collection.each(this.add);
+      this.collection.bind('add', this.add);
+      this.collection.bind('remove', this.remove);
+      return this.collection.bind('reset', this.reset);
+    };
+    UpdatingCollectionView.prototype.reset = function() {
+      var view, _i, _len, _ref;
+      if (this._rendered) {
         _ref = this._childViews;
-        _results = [];
         for (_i = 0, _len = _ref.length; _i < _len; _i++) {
           view = _ref[_i];
-          _results.push(this.renderChildView(view));
+          $(view.el).remove();
         }
-        return _results;
-      };
-      UpdatingCollectionView.prototype.renderChildView = function(childView) {
-        $(this.el).append(childView.el);
-        return childView.render;
-      };
-      return UpdatingCollectionView;
-    })();
-    TaskTableEntryView = (function() {
-      __extends(TaskTableEntryView, Backbone.View);
-      function TaskTableEntryView() {
-        this.render = __bind(this.render, this);
-        TaskTableEntryView.__super__.constructor.apply(this, arguments);
       }
-      TaskTableEntryView.prototype.tagName = $('#task-table-entry-template')[0].nodeName;
-      TaskTableEntryView.prototype.template = _.template($('#task-table-entry-template').html());
-      TaskTableEntryView.prototype.initialize = function(options) {
-        TaskTableEntryView.__super__.initialize.call(this, options);
-        return this.render();
-      };
-      TaskTableEntryView.prototype.render = function() {
-        var content;
-        console.log("Rendering", this, "to", this.el);
-        content = this.model.toJSON();
-        return $(this.el).html(this.template(content));
-      };
-      return TaskTableEntryView;
-    })();
-    TaskTableView = (function() {
-      __extends(TaskTableView, UpdatingCollectionView);
-      function TaskTableView() {
-        TaskTableView.__super__.constructor.apply(this, arguments);
+      this.collection.each(__bind(function(model) {
+        return this.add(model);
+      }, this));
+      return null;
+    };
+    UpdatingCollectionView.prototype.add = function(model) {
+      var childView;
+      childView = new this.childViewConstructor({
+        model: model
+      });
+      this._childViews.push(childView);
+      if (this._rendered) {
+        this.renderChildView(childView);
       }
-      TaskTableView.prototype.childViewConstructor = TaskTableEntryView;
-      TaskTableView.prototype.initialize = function(options) {
-        TaskTableView.__super__.initialize.call(this, options);
-        this.el = app.activePage();
-        return this.render();
-      };
-      return TaskTableView;
-    })();
-    HomeView = (function() {
-      __extends(HomeView, Backbone.View);
-      function HomeView() {
-        this.render = __bind(this.render, this);
-        HomeView.__super__.constructor.apply(this, arguments);
+      return reapplyStyles(this.el);
+    };
+    UpdatingCollectionView.prototype.remove = function(model) {
+      var viewToRemove;
+      viewToRemove = _(this._childViews).detect(function(cv) {
+        return cv.model === model;
+      });
+      this._childViews = _(this._childViews).without(viewToRemove);
+      if (this._rendered) {
+        return $(viewToRemove.el).remove();
       }
-      HomeView.prototype.el = $('#home');
-      HomeView.prototype.initialize = function(options) {
-        HomeView.__super__.initialize.call(this, options);
-        this.el = app.activePage();
-        return this.render();
-      };
-      HomeView.prototype.render = function() {
-        return this.$('[data-role=content]').text('Loaded');
-      };
-      return HomeView;
-    })();
-    return Backbone.history.start();
+    };
+    UpdatingCollectionView.prototype.render = function() {
+      var view, _i, _len, _ref;
+      this._rendered = true;
+      _ref = this._childViews;
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        view = _ref[_i];
+        this.renderChildView(view);
+      }
+      return reapplyStyles(this.el);
+    };
+    UpdatingCollectionView.prototype.renderChildView = function(childView) {
+      $(this.el).append(childView.el);
+      return childView.render;
+    };
+    return UpdatingCollectionView;
+  })();
+  TaskTableView = (function() {
+    __extends(TaskTableView, UpdatingCollectionView);
+    function TaskTableView() {
+      TaskTableView.__super__.constructor.apply(this, arguments);
+    }
+    TaskTableView.prototype.childViewConstructor = TaskTableEntryView;
+    TaskTableView.prototype.initialize = function() {
+      return TaskTableView.__super__.initialize.call(this, arguments);
+    };
+    return TaskTableView;
+  })();
+  TaskEventAddView = (function() {
+    __extends(TaskEventAddView, Backbone.View);
+    function TaskEventAddView() {
+      this.render = __bind(this.render, this);
+      TaskEventAddView.__super__.constructor.apply(this, arguments);
+    }
+    TaskEventAddView.prototype.initialize = function() {
+      TaskEventAddView.__super__.initialize.call(this, arguments);
+      return state.bind('change', this.render);
+    };
+    TaskEventAddView.prototype.render = function() {
+      if (!state.get('username')) {
+        return $(this.el).text("You must log in below before adding activity to a task.");
+      } else {
+        return $(this.el).text("Adding activity as " + (state.get('username')) + " to task: \n" + (state.get('task_title')));
+      }
+    };
+    return TaskEventAddView;
+  })();
+  HomeView = (function() {
+    __extends(HomeView, Backbone.View);
+    function HomeView() {
+      this.render = __bind(this.render, this);
+      HomeView.__super__.constructor.apply(this, arguments);
+    }
+    HomeView.prototype.initialize = function() {
+      HomeView.__super__.initialize.call(this, arguments);
+      state.bind('change', this.render);
+      return this.render();
+    };
+    HomeView.prototype.render = function() {
+      if (state.get('username')) {
+        return this.$('h2').text("Welcome, " + (state.get('username')) + "!");
+      } else {
+        return this.$('h2').text("Welcome to the Posterizer!");
+      }
+    };
+    return HomeView;
+  })();
+  $(function() {
+    var views;
+    views = [];
+    views.push(new HomeView({
+      el: $('#home').find('.content')
+    }));
+    views.push(new TaskTableView({
+      el: $('#task-table'),
+      collection: tasksWithEventCounts
+    }));
+    tasksWithEventCounts.fetch();
+    views.push(new TaskEventAddView({
+      el: $('#add-activity').find('.content')
+    }));
+    return $('body').live('pagecreate', function() {
+      var view, _i, _len, _results;
+      _results = [];
+      for (_i = 0, _len = views.length; _i < _len; _i++) {
+        view = views[_i];
+        _results.push(view.render());
+      }
+      return _results;
+    });
   });
 }).call(this);
